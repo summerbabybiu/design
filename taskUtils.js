c7cce271f56a9216bed646ea7d51c6d4dd007f70
王志龙 <991405736qq@gmail.com>
2016-05-11 10:25:34 +0800

    task and schedule mail

--- taskUtils.js
+++ taskUtils.js
@@ -2,6 +2,7 @@
  * Created by wangzhilong on 16/5/9.
  */
 var AV = require('avoscloud-sdk');
+var TASK_NAME = ['完成10节HTML课程','完成全部HTML课程','完成5节JavaScript课程','完成全部JavaScript课程'];
 
 var TaskUtils = {
   allTaskForUser: function(userid,callback) {
@@ -21,16 +22,66 @@ var TaskUtils = {
       callback(err, null);
     });
   },
+  checkTaskStatus: function(userid,callback){
+    var verfyFunc = this.verifyTaskStatusForUser;
+    var query = new AV.Query('_User');
+    query.get(userid).then(function(user){
+      console.log(user);
+      var name = user.get("username");
+      var query = new AV.Query('Record');
+      query.equalTo('user',name);
+      query.find().then(function(result){
+        result.forEach(function(obj){
+          verfyFunc(obj,user);
+        });
+      },function(err){
+        callback(err);
+      });
+
+    },function(err){
+      callback(err);
+    });
+  },
+  verifyTaskStatusForUser: function(record,user){
+    var completeTaskFunc = TaskUtils.completeTask;
+    var kind = record.get("courseKind");
+    var chapter = parseInt(record.get("chapter"));
+    var complete = record.get("finish");
+    var needUpdate = false;
+    var query = new AV.Query("UserTask");
+    var taskname = null;
+    if (kind == "HTML") {
+      if (chapter >= 10) {
+        taskname = TASK_NAME[0];
+        completeTaskFunc(user.id,taskname,function(err){});
+      }
+      if (complete) {
+        taskname = TASK_NAME[1];
+        completeTaskFunc(user.id,taskname,function(err){});
+      }
+    }
+    if(kind == "JS") {
+      if(chapter >= 5) {
+        taskname = TASK_NAME[2];
+        completeTaskFunc(user.id,taskname,function(err){});
+      }
+      if(complete) {
+        taskname = TASK_NAME[3];
+        completeTaskFunc(user.id,taskname,function(err){});
+      }
+    }
+  },
   completeTask: function(userid,taskid,callback) {
     var query = new AV.Query('UserTask');
     query.equalTo('user',userid);
-    query.equalTo('task',taskid);
+    query.equalTo('taskname',taskid);
     query.find().then(function(results){
       if(results.length > 0) {
-        results[0].complete = true;
+        results[0].set('complete',true);
         results[0].save().then(function(){
           callback(null);
         },function(err){
+          console.log(err);
           callback(err);
         });
       }
--- test.js
+++ test.js
@@ -5,6 +5,7 @@
 var AV = require('avoscloud-sdk');
 
 AV.initialize('iuO5g66bCpCVIhnRtQnmn3YA-gzGzoHsz', 'xkRJahD7klcYeHQ3BDVbbwDS');
+var taskUtil = require('./taskUtils');
 //
 //mailUtils.send({
 //  subject:"hello",
@@ -14,21 +15,41 @@ AV.initialize('iuO5g66bCpCVIhnRtQnmn3YA-gzGzoHsz', 'xkRJahD7klcYeHQ3BDVbbwDS');
 //});
 var UserTask = AV.Object.extend('UserTask');
 
-//var Task = AV.Object.extend("Task");
-var query = new AV.Query('Task');
-query.find().then(function(results){
-  if(results.length > 0) {
-    results.forEach(function(obj){
-      var usertask = new UserTask();
-      usertask.set("user","5705111479bc44004c42436c596804755");
-      usertask.set("taskid",obj.id);
-      usertask.set("taskname",obj.get("name"));
-      usertask.set("complete",false);
-      usertask.save().then(function(){},function(err){
-        console.log(err);
-      });
-    });
-  }
-},function(err){
-  console.log(err);
+////var Task = AV.Object.extend("Task");
+//var query = new AV.Query('Task');
+//query.find().then(function(results){
+//  if(results.length > 0) {
+//    results.forEach(function(obj){
+//      var usertask = new UserTask();
+//      usertask.set("user","5705111479bc44004c42436c");
+//      usertask.set("taskid",obj.id);
+//      usertask.set("taskname",obj.get("name"));
+//      usertask.set("complete",false);
+//      usertask.save().then(function(){},function(err){
+//        console.log(err);
+//      });
+//    });
+//  }
+//},function(err){
+//  console.log(err);
+//});
+//taskUtil.checkTaskStatus('5705111479bc44004c42436c',function(err){
+//  console.log(err);
+//});
+//var schedule = require("node-schedule");
+//var rule = schedule.RecurrenceRule();
+//rule.dayOfWeek = [0, new schedule.Range(1, 6)];
+//rule.hour = 1;
+//rule.minute = 0;
+//schedule.scheduledJobs(rule,function(){
+//  console.log()
+//});
+var schedule = require('node-schedule');
+var rule = new schedule.RecurrenceRule();
+rule.dayOfWeek = [0, new schedule.Range(4, 6)];
+rule.hour = 17;
+rule.minute = 0;
+
+var j = schedule.scheduleJob(rule, function(){
+  console.log('Today is recognized by Rebecca Black!');
 });
